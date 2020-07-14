1b6b36716dbde9059710face66e4857a2187b11f
Arie Blumenzweig <blumzi@013.net>
2020-07-14 10:30:43 +0300

    Working

--- Scripts/Wise/register.bat
+++ Scripts/Wise/register.bat
@@ -1,8 +1,9 @@
 @echo off
 
-C:\Windows\SysWoW\regsvr32 Wise.Ascom.wsc
-C:\Windows\SysWoW\regsvr32 Wise.Camera.wsc
-C:\Windows\SysWoW\regsvr32 Wise.DliPowerSwitch.wsc
-C:\Windows\SysWoW\regsvr32 Wise.Dome.wsc
-C:\Windows\SysWoW\regsvr32 Wise.Http.wsc
-C:\Windows\SysWoW\regsvr32 Wise.Util.wsc
+C:\Windows\SysWoW\regsvr32 /s Wise.Ascom.wsc
+C:\Windows\SysWoW\regsvr32 /s Wise.Camera.wsc
+C:\Windows\SysWoW\regsvr32 /s Wise.DliPowerSwitch.wsc
+C:\Windows\SysWoW\regsvr32 /s Wise.Dome.wsc
+C:\Windows\SysWoW\regsvr32 /s Wise.Http.wsc
+C:\Windows\SysWoW\regsvr32 /s Wise.Util.wsc
+C:\Windows\SysWoW\regsvr32 /s Wise.Tele.wsc
