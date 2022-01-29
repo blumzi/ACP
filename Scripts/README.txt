7c96a8eb62609a00d0a7460816551ed501f04800
theblumz@gmail.com <theblumz@gmail.com>
2022-01-29 11:55:21 +0200

    Works

---
+++ Scripts/README.txt
@@ -0,0 +1,4 @@
+To register the .wsc files do:
+
+	Right click on register.bat
+	Run As Administrator
---
+++ Scripts/Wise/register.bat
@@ -0,0 +1,7 @@
+C:\Windows\SysWOW64\regsrv32.exe /s Wise.Ascom.wsc
+C:\Windows\SysWOW64\regsrv32.exe /s Wise.Camera.wsc
+C:\Windows\SysWOW64\regsrv32.exe /s Wise.Dli.wsc
+C:\Windows\SysWOW64\regsrv32.exe /s Wise.Dome.wsc
+C:\Windows\SysWOW64\regsrv32.exe /s Wise.Http.wsc
+C:\Windows\SysWOW64\regsrv32.exe /s Wise.Tele.wsc
+C:\Windows\SysWOW64\regsrv32.exe /s Wise.Util.wsc
